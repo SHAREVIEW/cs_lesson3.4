fad500d8615e9bf79dd6a3e80569249d6af54d8e
david <2657070472@qq.com>
2018-10-29 15:58:47 +0800

    do 循环

--- cs_lesson3.4/Program.cs
+++ cs_lesson3.4/Program.cs
@@ -10,6 +10,44 @@ namespace cs_lesson3._4
     {
         static void Main(string[] args)
         {
+
+            //do循环
+
+            double balance, interestRate, targetBalance;
+            Console.WriteLine("What's your current balance?");
+            balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What's your current annual interest rate (in %)?");
+            interestRate = 1 + Convert.ToDouble(Console.ReadLine()) / 100.0;
+            Console.WriteLine("What balance would you like to have ?");
+            targetBalance = Convert.ToDouble(Console.ReadLine());
+
+            int totalYears = 0;
+            do
+            {
+                balance *= interestRate;
+                ++totalYears;
+            }
+            while (balance < targetBalance);
+            Console.WriteLine("In {0} year{1} you'll have a balance of {2}.", 
+                totalYears, totalYears == 1? "" : "s", balance);  //1?"":s三元运算，1时空，！=1时s 
+            Console.ReadKey();
+
+
+
+            //do循环至少要执行1次,下面while表达式
+            /*
+             * int totalYears = 0;
+               while (balance < targetBalance)
+               {
+                 balance *= interestRate;
+                ++totalYears;
+               }
+               Console.WriteLine("In {0} year{1} you'll have a balance of {2}.",
+               totalYears, totalYears == 1 ? "" : "s", balance);
+               Console.ReadKey();
+               
+                 */
         }
+
     }
 }
